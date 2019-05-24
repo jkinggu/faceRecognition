1890137b3fd05341ab2691c797526bdc7922eb20
jkinggu <j.kinggu@aliyun.com>
2019-05-24 14:41:21 +0800

    ui

---
+++ .svn/pristine/0d/0d145107063cfbb7833d03a4c569fd7699daf2a8.svn-base
@@ -0,0 +1,2 @@
+# faceRecognition
+#此项目使用opencv的人脸识别，和身份证识别做的一套认证系统
--- src/com/dx/util/Const.java
+++ src/com/dx/util/Const.java
@@ -6,5 +6,8 @@ public class Const {
     public static final Integer CV_CAP_PROP_FRAME_WIDTH=800;
     public static final Integer CV_CAP_PROP_FRAME_HEIGHT=600;
     public static final Integer CV_CAP_PROP_FPS=30;
+    public static final String  FONT_TYPE="Î¢ÈíÑÅºÚ";
+    public static final Integer  FONT_SIZE=13;
+    
     
 }
